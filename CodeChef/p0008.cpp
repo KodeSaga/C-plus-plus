d9c5de5343d625652420ec5183ada4c11ac70fbe
KodeSaga <kode.drama@gmail.com>
2019-02-21 23:44:35 +0530

    p0008 codechef Completed

---
+++ CodeChef/p0008.cpp
@@ -0,0 +1,26 @@
+//[Complete]Problem statement - https://www.codechef.com/NCC2019/problems/NCC1903
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        long int t,n,k,a,count=0;
+        cin >> t;
+        for(int i=0;i<t;i++){
+            cin >> n >> k;
+            count=0;
+            for(int j=0;((j<k) && (n>=0)) ;j++){
+	                cin>>a;
+                    if(a <= n){
+                            n -= a;
+                            count++;
+                    }
+            }
+            cout << count << endl;
+        }
+	return 0;
+}
---
+++ CodeChef/p0009.cpp
@@ -0,0 +1,34 @@
+//[inComplete]Problem statement - https://www.codechef.com/NCC2019/problems/NCC1903
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        int q,n1,n2;
+        long long l,r;
+        vector <long long> a;
+        cin >> q;
+        for(int i=0;i<t;i++){
+            cin >> l >> r >> n1 >> n2;
+            for(int j=l;j<=r;j++){
+	                cin>>a;
+                    if(a <= n){
+                            n -= a;
+                            count++;
+                    }
+            }
+            if( )
+                    cout << "Draw" << "\n";
+            else if( )
+                    cout << "Onkar" << "\n";
+                 else
+                    cout << "Krushna" << "\n";
+                        
+                    
+       }
+	return 0;
+}
