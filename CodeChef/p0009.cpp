fe855c6811bed28234e877ab1d607aadbb450466
KodeSaga <kode.drama@gmail.com>
2019-02-23 12:28:04 +0530

    p0009 - DigitSum solved needs efficiency improvement.

--- CodeChef/p0009.cpp
+++ CodeChef/p0009.cpp
@@ -27,10 +27,10 @@ int main() {
         for(int i=0;i<q;i++){
             n1count=0,n2count=0;
             //cin >> l >> r >> n1 >> n2;
-            scanf("%lld %lld %d%d",&l,&r,&n1,&n2);
+            scanf("%lld %lld %d %d",&l,&r,&n1,&n2);
             for(long long j=l;j<=r;j++){
-                    a = delta(j);
-                    (a == n1) ? n1count++ : 1;
+                        a = delta(j);
+                        (a == n1) ? n1count++ : 1;
 	                (a == n2) ? n2count++ : 1;
             }
             if(n1count == n2count)
--- readme.md
+++ readme.md
@@ -8,6 +8,6 @@
 | p0006 | **inComplete** | Codechef | Find if n elements can be grouped in 3 such that sum of each group is same. |
 | p0007 | **inComplete** | Codechef | Largest substring common between 2 variable length strings. |
 | p0008 | Complete | Codechef | Contraint based counting |
-| p0009 | **inComplete** | Codechef | Maximiztion under constraints |
+| p0009 | **inComplete** | Codechef | Solved but Need to improve algo. Digitsum  |
 
 If you have a more elegant solution to any of these , do let me know at kode.drama@gmail.com
